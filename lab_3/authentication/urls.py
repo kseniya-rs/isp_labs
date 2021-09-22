516ddfaf4607debd42ca0bc7808bdb753cd88822
kseniya-rs <63047743+kseniya-rs@users.noreply.github.com>
2021-09-22 19:04:18 +0300

    Delete lab_3 directory

--- lab_3/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LoginView
-
-urlpatterns = [
-    path('signup', views.signup, name='signup'),
-    path('login', LoginView.as_view(redirect_authenticated_user=True), name='login'),
-    path('change_password', views.change_password, name="change_password")
-]
--- lab_3/noty/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('authentication/', include('authentication.urls')),
-    path('ckeditor', include('ckeditor_uploader.urls')),
-    path('', include('note.urls'))
-]
